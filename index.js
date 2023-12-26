f24a1c0f5c20d6682ec6acf06529a04515922950
Prakash Pramanik <prakashpramanik280@gmail.com>
2023-12-26 15:11:32 +0530

    add another functionality

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set( 'layout extractScripts',true)
+
+app.use(express.static('public'));
+
 
 // use express router
 app.use('/', require('./routes/index'));
