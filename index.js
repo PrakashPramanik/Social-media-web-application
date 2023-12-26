0d6221ac64d8b6dd43267fa805bd31e9aa3eec74
Prakash Pramanik <prakashpramanik280@gmail.com>
2023-12-26 11:19:44 +0530

    add a controller or a home

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+// use express router
+app.use('/', require('./routes/index'));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
