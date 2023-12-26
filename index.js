ca08cf300817043e3bc0c8a6773bafadf61a92c0
Prakash Pramanik <prakashpramanik280@gmail.com>
2023-12-26 11:45:14 +0530

    add another functionality

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 // use express router
 app.use('/', require('./routes/index'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
